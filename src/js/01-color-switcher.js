4e821c06fcf0fc1a839f21b8e99d3dbff1f7515e
Vadymshairan <95377527+Vadymshairan@users.noreply.github.com>
2022-08-28 04:27:22 +0300

    Задание 1 - переключатель цветов

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+stopBtn.disabled = true;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startBgChangeColor() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function stopBgChangeColor() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+startBtn.addEventListener('click', startBgChangeColor);
+stopBtn.addEventListener('click', stopBgChangeColor);
